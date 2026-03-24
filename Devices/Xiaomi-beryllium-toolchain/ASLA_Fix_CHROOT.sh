e7ea98764689fab3b635dd6a4e1827e385a7f4f0
PastorCatto <zanderiscool185@gmail.com>
2026-03-23 22:11:43 -0700

    Qualcomm Software Stack Update
    We established communication with TrustZone, and Ubuntu is accepting the firmware blobs!
    
    -added firmware checker to make sure everything is installed before sealing image
    
    -added fixes to qrtr-ns (built into kernel, will test more tomorrow)

--- Devices/Xiaomi-beryllium-toolchain/ASLA_Fix_CHROOT.sh
+++
@@ -1,6 +0,0 @@
-echo "--- Fixing Audio (ALSA UCM) ---"
-apt-get install -y alsa-ucm-conf
-cd /tmp
-git clone --depth 1 https://gitlab.com/sdm845-mainline/alsa-ucm-conf.git sdm845-ucm
-cp -r sdm845-ucm/ucm2/conf.d/sm845 /usr/share/alsa/ucm2/conf.d/
-cp -r sdm845-ucm/ucm2/sm845 /usr/share/alsa/ucm2/
